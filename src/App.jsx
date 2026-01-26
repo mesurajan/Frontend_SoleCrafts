83176af046a80ee33bdc4cd032df46dc6917c100
mesurajan <surajansth1995@gmail.com>
2026-01-26 16:36:11 +0545

    clean setup completed on day 01 of session

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-red-800'>
+      <h1 className='text-4xl text-blue-500 text-center p-10'>Testing the tailwind css</h1>
+      <p className='text-2xl text-blue-500 text-center p-10 ' >
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi 
+        omnis voluptatibus ipsum eos quidem delectus ducimus libero, assumenda 
+        iusto quos enim, maiores veniam ex earum corrupti at eius repudiandae
+         sit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi 
+        omnis voluptatibus ipsum eos quidem delectus ducimus libero, assumenda 
+        iusto quos enim, maiores veniam ex earum corrupti at eius repudiandae
+         sit.  
+      </p>
+      </div>
+  )
+}
+
+export default App
